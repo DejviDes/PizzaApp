a43cf337e89a1379ed7faf161520615518d93dd9
DejviDes <trebaticky.david@outlook.com>
2025-01-02 22:16:04 +0100

    setup

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
