af085903f1b77203b57b5088477cdb9c96ddacb6
DejviDes <trebaticky.david@outlook.com>
2025-02-16 17:43:37 +0100

    refactor HeroSection and Navbar components; improve structure and styling

--- app/src/components/HeroSection.jsx
+++ app/src/components/HeroSection.jsx
@@ -1,169 +1,178 @@
+import styled from "styled-components";
 import HeroImg from "../assets/img/heroImg.png";
 import HeroSubImg from "../assets/img/heroSubImg.png";
-import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import {FaFacebookF, FaInstagram, FaTwitter} from "react-icons/fa";
+
+const HeroContainer = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background-color: white;
+`;
+
+const HeroSubContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    align-items: center;
+    max-width: 72rem;
+    margin: 0 auto 4rem;
+    padding-top: 13rem;
+    padding-left: 2.5rem;
+    paddin-right: 2.5rem;
+`;
+
+const HeroContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const HeroHeading = styled.h1`
+    font-weight: 500;
+    font-family: serif;
+    font-size: 3rem;
+    line-height: 1;
+    margin-bottom: 4rem;
+    max-width: 24rem;
+`;
 
 function Herosection() {
-  return (
-    <div
-      style={{ width: "100%", minHeight: "100vh", backgroundColor: "white" }}
-    >
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(2, minmax(0, 1fr))",
-          alignItems: "center",
-          maxWidth: "72rem",
-          margin: "0 auto",
-          marginBottom: "4rem",
-          paddingTop: "13rem",
-          paddingLeft: "2.5rem",
-          paddingRight: "2.5rem",
-        }}
-      >
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <h1
-            style={{
-              fontWeight: 500,
-              fontFamily: "serif",
-              fontSize: "3rem",
-              lineHeight: "1",
-              marginBottom: "4rem",
-              maxWidth: "24rem",
-            }}
-          >
-            We provide the best food for you
-          </h1>
-          <p
-            style={{
-              color: "#4b5563",
-              fontSize: "0.875rem",
-              maxWidth: "20rem",
-              lineHeight: "1.75rem",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              gap: "1.25rem",
-              marginTop: "2.25rem",
-            }}
-          >
-            <a
-              style={{
-                backgroundColor: "#000000",
-                fontFamily: "serif",
-                filter: "drop-shadow(var(0 3px 3px rgb(0 0 0 / 0.12)))",
-                color: "white",
-                borderTopLeftRadius: "1rem",
-                borderBottomRightRadius: "1rem",
-                borderTopRightRadius: "0.375rem",
-                borderBottomLeftRadius: "0.375rem",
-                paddingTop: "1rem",
-                paddingBottom: "1rem",
-                paddingLeft: "1.75rem",
-                paddingRight: "1.75rem",
-                textWrap: "nowrap",
-              }}
-              href=""
-            >
-              Menu
-            </a>
-            <a
-              style={{
-                backgroundColor: "#EA6D27",
-                fontFamily: "serif",
-                filter: "drop-shadow(var(0 3px 3px rgb(0 0 0 / 0.12)))",
-                color: "white",
-                borderTopLeftRadius: "1rem",
-                borderBottomRightRadius: "1rem",
-                borderTopRightRadius: "0.375rem",
-                borderBottomLeftRadius: "0.375rem",
-                paddingTop: "1rem",
-                paddingBottom: "1rem",
-                paddingLeft: "1.75rem",
-                paddingRight: "1.75rem",
-                textWrap: "nowrap",
-              }}
-              href=""
-            >
-              Book a table
-            </a>
-          </div>
-          <div style={{ marginTop: "4rem" }}>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                gap: "1rem",
-                color: "#4b5563",
-                alignItems: "center",
-              }}
-            >
-              <div
-                style={{
-                  cursor: "pointer",
-                  borderRadius: "9999px",
-                  border: "1px solid black",
-                  padding: "0.5rem",
-                  color: "black",
-                }}
-              >
-                <FaFacebookF />
-              </div>
-              <div
-                style={{
-                  cursor: "pointer",
-                  borderRadius: "9999px",
-                  border: "1px solid black",
-                  padding: "0.5rem",
-                  color: "black",
-                }}
-              >
-                <FaInstagram />
-              </div>
-              <div
-                style={{
-                  cursor: "pointer",
-                  borderRadius: "9999px",
-                  border: "1px solid black",
-                  padding: "0.5rem",
-                  color: "black",
-                }}
-              >
-                <FaTwitter />
-              </div>
-              <div
-                style={{
-                  height: "0px",
-                  width: "8rem",
-                  border: "1px solid #e5e7eb",
-                }}
-              ></div>
-            </div>
-          </div>
-        </div>
-        <div
-          style={{
-            position: "relative",
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-end",
-          }}
-        >
-          <img src={HeroImg} alt="" />
-          <img
-            style={{ position: "absolute", bottom: "2rem", left: "-12rem" }}
-            src={HeroSubImg}
-            alt=""
-          />
-        </div>
-      </div>
-    </div>
-  );
+    return (
+        <HeroContainer>
+            <HeroSubContainer>
+                <HeroContent>
+                    <HeroHeading>
+                        We provide the best food for you
+                    </HeroHeading>
+                    <p
+                        style={{
+                            color: "#4b5563",
+                            fontSize: "0.875rem",
+                            maxWidth: "20rem",
+                            lineHeight: "1.75rem",
+                        }}
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "1.25rem",
+                            marginTop: "2.25rem",
+                        }}
+                    >
+                        <a
+                            style={{
+                                backgroundColor: "#000000",
+                                fontFamily: "serif",
+                                filter: "drop-shadow(var(0 3px 3px rgb(0 0 0 / 0.12)))",
+                                color: "white",
+                                borderTopLeftRadius: "1rem",
+                                borderBottomRightRadius: "1rem",
+                                borderTopRightRadius: "0.375rem",
+                                borderBottomLeftRadius: "0.375rem",
+                                paddingTop: "1rem",
+                                paddingBottom: "1rem",
+                                paddingLeft: "1.75rem",
+                                paddingRight: "1.75rem",
+                                textWrap: "nowrap",
+                            }}
+                            href=""
+                        >
+                            Menu
+                        </a>
+                        <a
+                            style={{
+                                backgroundColor: "#EA6D27",
+                                fontFamily: "serif",
+                                filter: "drop-shadow(var(0 3px 3px rgb(0 0 0 / 0.12)))",
+                                color: "white",
+                                borderTopLeftRadius: "1rem",
+                                borderBottomRightRadius: "1rem",
+                                borderTopRightRadius: "0.375rem",
+                                borderBottomLeftRadius: "0.375rem",
+                                paddingTop: "1rem",
+                                paddingBottom: "1rem",
+                                paddingLeft: "1.75rem",
+                                paddingRight: "1.75rem",
+                                textWrap: "nowrap",
+                            }}
+                            href=""
+                        >
+                            Book a table
+                        </a>
+                    </div>
+                    <div style={{marginTop: "4rem"}}>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                gap: "1rem",
+                                color: "#4b5563",
+                                alignItems: "center",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    cursor: "pointer",
+                                    borderRadius: "9999px",
+                                    border: "1px solid black",
+                                    padding: "0.5rem",
+                                    color: "black",
+                                }}
+                            >
+                                <FaFacebookF/>
+                            </div>
+                            <div
+                                style={{
+                                    cursor: "pointer",
+                                    borderRadius: "9999px",
+                                    border: "1px solid black",
+                                    padding: "0.5rem",
+                                    color: "black",
+                                }}
+                            >
+                                <FaInstagram/>
+                            </div>
+                            <div
+                                style={{
+                                    cursor: "pointer",
+                                    borderRadius: "9999px",
+                                    border: "1px solid black",
+                                    padding: "0.5rem",
+                                    color: "black",
+                                }}
+                            >
+                                <FaTwitter/>
+                            </div>
+                            <div
+                                style={{
+                                    height: "0px",
+                                    width: "8rem",
+                                    border: "1px solid #e5e7eb",
+                                }}
+                            ></div>
+                        </div>
+                    </div>
+                </HeroContent>
+                <div
+                    style={{
+                        position: "relative",
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "flex-end",
+                    }}
+                >
+                    <img src={HeroImg} alt=""/>
+                    <img
+                        style={{position: "absolute", bottom: "2rem", left: "-12rem"}}
+                        src={HeroSubImg}
+                        alt=""
+                    />
+                </div>
+            </HeroSubContainer>
+        </HeroContainer>
+    );
 }
 
 export default Herosection;
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,82 +1,78 @@
-import Logo from "../assets/img/logo.png";
+import styled from "styled-components";
+import LogoImg from "../assets/img/logo.png";
+
+const NavContainer = styled.nav`
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background-color: white;
+    z-index: 10;
+`;
+
+const NavSubContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 3rem 0;
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.75rem;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const Logo = styled.img`
+    margin-bottom: 1.25rem;
+`;
+
+const LogoText = styled.h2`
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+    font-weight: 600;
+`;
+
+const NavLinks = styled.ul`
+    display: flex;
+    flex-direction: row;
+    gap: 2.5rem;
+    color: #4b5563;
+`;
+
+const NavItem = styled.li`
+    cursor: pointer;`;
+
+const NavButton = styled.a`
+    background-color: #EA6D27;
+    font-family: serif;
+    filter: drop-shadow(0 3px 3px rgb(0 0 0 / 0.12));
+    color: white;
+    border-radius: 1rem 0.375rem 1rem 0.375rem;
+    padding: 1.25rem 1.75rem;
+    white-space: nowrap;
+`;
 
 function Navbar() {
-  return (
-    <nav
-      style={{
-        position: "fixed",
-        top: 0,
-        width: "100%",
-        backgroundColor: "white",
-        zIndex: 10,
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          paddingTop: "3rem",
-          paddingBottom: "3rem",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "0.75rem",
-            alignItems: "center",
-            cursor: "pointer",
-          }}
-        >
-          <img style={{ marginBottom: "1.25rem" }} src={Logo} alt="" />
-          <h2
-            style={{
-              fontSize: "1.875rem",
-              lineHeight: "2.25rem",
-              fontWeight: 600,
-            }}
-          >
-            restaurant
-          </h2>
-        </div>
-        <ul
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "2.5rem",
-            color: "#4b5563",
-          }}
-        >
-          <li style={{ cursor: "pointer" }}>Menu</li>
-          <li style={{ cursor: "pointer" }}>Events</li>
-          <li style={{ cursor: "pointer" }}>Gallery</li>
-          <li style={{ cursor: "pointer" }}>About</li>
-          <li style={{ cursor: "pointer" }}>Contact</li>
-        </ul>
-        <a
-          style={{
-            backgroundColor: "#EA6D27",
-            fontFamily: "serif",
-            filter: "drop-shadow(0 3px 3px rgb(0 0 0 / 0.12))",
-            color: "white",
-            borderTopLeftRadius: "1rem",
-            borderBottomRightRadius: "1rem",
-            borderTopRightRadius: "0.375rem",
-            borderBottomLeftRadius: "0.375rem",
-            paddingTop: "1.25rem",
-            paddingBottom: "1.25rem",
-            paddingLeft: "1.75rem",
-            paddingRight: "1.75rem",
-            textWrap: "nowrap",
-          }}
-          href=""
-        >
-          Book a table
-        </a>
-      </div>
-    </nav>
-  );
+    return (
+        <NavContainer>
+            <NavSubContainer>
+                <LogoContainer>
+                    <Logo src={LogoImg} alt=""/>
+                    <LogoText>restaurant</LogoText>
+                </LogoContainer>
+                <NavLinks>
+                    <NavItem>Menu</NavItem>
+                    <NavItem>Events</NavItem>
+                    <NavItem>Gallery</NavItem>
+                    <NavItem>About</NavItem>
+                    <NavItem>Contact</NavItem>
+                </NavLinks>
+                <NavButton href="">Book a table</NavButton>
+            </NavSubContainer>
+        </NavContainer>
+    );
 }
 
 export default Navbar;
